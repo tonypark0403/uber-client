39cd79609cbc8a7bea47c21413db7b35fb03d387
Tony Park <tonypark0403@gmail.com>
2020-05-04 00:19:34 -0400

    applied apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
