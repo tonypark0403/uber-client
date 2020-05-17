fa411872ed67e448d8fb53f7cbb97d908a1980c8
Tony Park <tonypark0403@gmail.com>
2020-05-17 00:12:07 -0400

    added web socket for subscribing

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,53 +1,113 @@
-import ApolloClient, { Operation } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink, concat, Operation, split } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLink } from 'apollo-link-http';
+import { withClientState } from 'apollo-link-state';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { toast } from 'react-toastify';
 import config from './config';
 
-const client = new ApolloClient({
-  clientState: {
-    defaults: {
-      auth: {
-        __typename: 'Auth',
-        isLoggedIn: Boolean(localStorage.getItem(config.AUTH.TOKEN_COOKIE)),
-      },
+const getToken = () => {
+  const token = localStorage.getItem('jwt');
+  if (token) {
+    return token;
+  } else {
+    return '';
+  }
+};
+
+const cache = new InMemoryCache();
+
+const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
+  operation.setContext({
+    headers: {
+      jwt: getToken(),
     },
-    resolvers: {
-      Mutation: {
-        logUserIn: (_, { token }, { cache }) => {
-          localStorage.setItem(config.AUTH.TOKEN_COOKIE, token);
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: 'Auth',
-                isLoggedIn: true,
-              },
-            },
-          });
-          return null;
-        },
-        logUserOut: (_, __, { cache }) => {
-          localStorage.removeItem(config.AUTH.TOKEN_COOKIE);
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: 'Auth',
-                isLoggedIn: false,
-              },
-            },
-          });
-          return null;
-        },
-      },
-    },
-  },
-  request: async (operation: Operation) => {
-    // console.log('client operation:', operation.getContext());
-    operation.setContext({
-      headers: {
-        [config.AUTH.TOKEN_COOKIE]:
-          localStorage.getItem(config.AUTH.TOKEN_COOKIE) || '',
-      },
-    });
-  },
+  });
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
   uri: `http://localhost:4000${config.GRAPHQL.GRAPHQL_ENDPOINT}`,
-}); // my graphql endpoint
+});
+
+const wsLink = new WebSocketLink({
+  options: {
+    connectionParams: {
+      jwt: getToken(),
+    },
+    reconnect: true,
+  },
+  uri: `ws://localhost:4000${config.SUBSCRIPTION.SUBSCRIPTION_ENDPOINT}`,
+});
+
+const combinedLinks = split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message }) => {
+      toast.error(`Unexpected error: ${message}`);
+    });
+  }
+  if (networkError) {
+    toast.error(`Network error: ${networkError}`);
+  }
+});
+
+const localStateLink = withClientState({
+  cache,
+  defaults: {
+    auth: {
+      __typename: 'Auth',
+      isLoggedIn: Boolean(localStorage.getItem('jwt')),
+    },
+  },
+  resolvers: {
+    Mutation: {
+      logUserIn: (_, { token }, { cache: appCache }) => {
+        localStorage.setItem('jwt', token);
+        appCache.writeData({
+          data: {
+            auth: {
+              __typename: 'Auth',
+              isLoggedIn: true,
+            },
+          },
+        });
+        return null;
+      },
+      logUserOut: (_, __, { cache: appCache }) => {
+        localStorage.removeItem('jwt');
+        appCache.writeData({
+          data: {
+            auth: {
+              __typename: 'Auth',
+              isLoggedIn: false,
+            },
+          },
+        });
+        return null;
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([
+    errorLink,
+    localStateLink,
+    concat(authMiddleware, combinedLinks),
+  ]),
+});
 
 export default client;
